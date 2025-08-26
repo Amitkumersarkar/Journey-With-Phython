b96f3ebdf8522990b607b8b7bebfa1543225517e
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-27 00:14:30 +0600

    math in python

---
+++ Chapter01(Variable&Data Types)/Keyword.py
@@ -0,0 +1,17 @@
+# keywords are reserved words in python
+# such as [and ,else,in,return,as,while,del except,break,pass,if etc]
+# python is a case sensitive language
+
+# problem: print sum of two number
+
+num1 = 10
+num2 = 25
+sum = num1+num2
+print(sum)
+
+# multiple three number
+a = 5
+b = 10
+c = 3
+multi = a*b*c
+print(multi)

7051097c30e3994e616b18a363c1fff0e2f2f0cd
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-26 22:20:10 +0600

    added new file

---
+++ Chapter01/Variable.py
@@ -0,0 +1,5 @@
+# variable is container for a value (string,integer,float,boolean)
+# A variable behave as if it was the value it contains
+first_pyCode = " Programmer"
+print(first_pyCode)
+print(f"Hello{first_pyCode}")

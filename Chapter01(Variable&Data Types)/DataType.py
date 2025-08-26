af97e0c4643239057b080e551d3899e213fea421
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-27 00:05:12 +0600

    updated file

--- Chapter01(Variable&Data Types)/DataType.py
+++ Chapter01(Variable&Data Types)/DataType.py
@@ -4,3 +4,25 @@ gpa = 3.02
 print(type(name))
 print(type(age))
 print(type(gpa))
+
+
+# integer
+age = 24
+print(f"iam {age} years old")
+
+# float
+price = 599
+gpa = 3.5
+distance = 55.2
+
+print(f"the t-shirt price is {price} tk")
+print(f"my gpa is {gpa}")
+print(f"my campus distance {distance} k.m from my home")
+
+# boolean
+is_student = False
+print(f"are you a student?{is_student} ")
+if is_student:
+    print("your a student")
+else:
+    print("you are not a student")

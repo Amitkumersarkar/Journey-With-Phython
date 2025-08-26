2c4ba32c396d6628447db39b5de1a48de06e1071
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-27 00:44:20 +0600

    added operator in python

---
+++ Chapter01(Variable&Data Types)/Operator.py
@@ -0,0 +1,42 @@
+# an operator is a symbol that performs a certain operation between operands
+# such as :
+# Arithmetic Operator : (+,-,*,/,%,**)
+# example :
+a = 5
+b = 10
+multi = a*b
+print(multi)
+
+x = 10
+y = 20
+print(x+y)
+print(x-y)
+print(x/y)
+print(x % y)  # Remainder
+print(x**y)  # x^y (double star indicate power)
+# Relational Operator : (==,!=,>,<,>=,<=)
+# example :
+a = 20
+b = 10
+print(a == b)  # False
+print(a != b)  # True
+print(a >= b)
+print(a <= b)
+print(a > b)
+print(a < b)
+# Assignment Operator : (=,+=,-=,*=,/=,%=,**=)
+# example :
+a = 20
+a = a+10
+print(a)
+
+# or
+x = 10
+x += 10
+print(x)
+# or
+m = 5
+m *= 10
+print(m)
+# Logical Operators (not,and,or)
+# example:

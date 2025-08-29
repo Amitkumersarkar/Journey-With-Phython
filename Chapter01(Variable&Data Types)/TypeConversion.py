1612230c3df9eedd8f210ca887f55fe14796c1a8
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-30 02:21:58 +0600

    added type conversion

---
+++ Chapter01(Variable&Data Types)/TypeConversion.py
@@ -0,0 +1,18 @@
+# type conversion
+a = int("5")
+b = 4.5
+sum = a+b
+print(type(a))
+print(sum)
+# example 2
+val = input("Enter some values :")
+print(type(val), val)
+
+# example 3
+name = input("Enter your name :")
+age = int("Enter your age :")
+marks = float("Enter your marks : ")
+
+print(name)
+print(age)
+print(marks)

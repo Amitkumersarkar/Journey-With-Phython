e7335b99cd6196c78b469eb2a7206a7dfb0894ed
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-30 02:37:33 +0600

    added practice task

---
+++ PracTask2.py
@@ -0,0 +1,4 @@
+# WAP to input side of a square and print it's are
+
+side = float(input("enter square side :"))
+print("Area = ", side*side)
---
+++ PracTask3.py
@@ -0,0 +1,5 @@
+# WAP to input 2 floating point number and print their average
+
+a = int(input("Enter your first value : "))
+b = int(input("Enter your second value :"))
+print("The average number is : ", (a+b)/2)

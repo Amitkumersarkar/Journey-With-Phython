c04e33eece8f29618d8fb11d2561f21e643d1eab
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-30 02:41:57 +0600

    added type casting problem

---
+++ PracTask4.py
@@ -0,0 +1,7 @@
+# WAP to input 2 int numbers a and b.
+# print true if a is greater than or equal to b. if not print false
+
+a = int(input("enter value of a : "))
+b = int(input("enter value of b : "))
+
+print(a >= b)

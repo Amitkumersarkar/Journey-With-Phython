96a34af40072b7cb5217c8805710be082bda53d5
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-09-04 23:29:49 +0600

    added multi list

---
+++ Chapter-03(List&Tuple)/MultiList.py
@@ -0,0 +1,6 @@
+# list are mutable in python (mutable means changeable )
+# strings are immutable (unchangeable)
+student = ["Amit", 85.5, 283, "Dhaka"]
+print(student)
+student[0] = "Amrita"
+print(student)

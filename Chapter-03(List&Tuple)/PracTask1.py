d576fb392c4675eb19c39f9233faa10a9afdb7d6
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-09-07 01:29:21 +0600

    check list is palindrome or not

--- Chapter-03(List&Tuple)/PracTask1.py
+++ Chapter-03(List&Tuple)/PracTask1.py
@@ -1,3 +1,5 @@
+# WAP to ask the user to enter names of their 3 favorite movies & store them in a list
+
 movies = []
 movie1 = str(input("Enter your 1st fav movie :"))
 movie2 = str(input("Enter your 2nd fav movie :"))

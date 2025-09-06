b56fc57e9eaf00f6dba073b086a3dca3cd2d4031
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-09-07 01:17:35 +0600

    added tulpe practice task 1

---
+++ Chapter-03(List&Tuple)/PracTask1.py
@@ -0,0 +1,9 @@
+movies = []
+movie1 = str(input("Enter your 1st fav movie :"))
+movie2 = str(input("Enter your 2nd fav movie :"))
+movie3 = str(input("Enter your 3rd fav movie :"))
+
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
+print(movies)

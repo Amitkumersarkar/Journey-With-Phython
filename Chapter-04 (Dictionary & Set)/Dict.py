d97c49458fbc1fd18cf53a848e3ab48d6e419e59
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-10-07 22:32:39 +0600

    added dictonary and set in python

---
+++ Chapter-04 (Dictionary & Set)/Dict.py
@@ -0,0 +1,10 @@
+info = {
+    "Name": "Amit Sarkar",
+    "Age": 23,
+    "Is_Adult": True,
+    "Student": True,
+    "GPA": 3.01,
+    "Subject": ["CSE-231", "CSE-215"],
+    "Topics": ("Dict", "Set")
+}
+print(info)

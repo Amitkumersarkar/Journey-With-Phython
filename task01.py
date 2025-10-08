9473ee044fce2592c825b8879b3a9bf0e2bf92f4
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-10-08 19:34:01 +0600

    added some basic task

---
+++ task01.py
@@ -0,0 +1,5 @@
+store = {
+    "table": ["a piece of furniture", "list of facts & figures"],
+    "cat": "a small animal"
+}
+print(store)
---
+++ task03.py
@@ -0,0 +1,8 @@
+marks = {}
+x = int(input("enter py : "))
+marks.update({"phy": x})
+
+x = int(input("enter math : "))
+marks.update({"phy": x})
+
+print(marks)

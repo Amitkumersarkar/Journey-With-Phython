42a162b941c90dd4e54009f7e325597e8a2f9e70
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-10-08 17:16:51 +0600

    added dict values methods

---
+++ Chapter-04 (Dictionary & Set)/DictMethod.py
@@ -0,0 +1,10 @@
+student = {
+    "Name": "Amit sarkar",
+    "Subjects": {
+        "phy": 85,
+        "OS": 80,
+        "DBMS": 80
+    }
+}
+print(student.values())
+print(list(student.values()))
--- Chapter-04 (Dictionary & Set)/NastedDict.py
+++ Chapter-04 (Dictionary & Set)/NastedDict.py
@@ -8,6 +8,7 @@ student = {
 }
 # print(student)
 print(student["Subjects"]["OS"])
+# declared dict keys method
 print(student.keys())
 # type_casting
 print(list(student.keys()))

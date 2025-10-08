9932ba209be9037ebc6e43ccb9b28240eb21f668
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-10-08 17:21:03 +0600

    added items method in dict py

---
+++ Chapter-04 (Dictionary & Set)/DictItemsMethod.py
@@ -0,0 +1,10 @@
+student = {
+    "Name": "Amit sarkar",
+    "Subjects": {
+        "phy": 85,
+        "OS": 80,
+        "DBMS": 80
+    }
+}
+# type cast in list items methods
+print(list(student.items()))

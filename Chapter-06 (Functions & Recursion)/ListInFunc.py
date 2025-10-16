bb10e344a4c23b5d14d9ec97fb021ffa50ed0749
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-10-16 15:31:19 +0600

    added new list in function

---
+++ Chapter-06 (Functions & Recursion)/ListInFunc.py
@@ -0,0 +1,8 @@
+cities = ["Dhaka", "Tangail", "Mirzapur", "Chittagoan", "Comilla", "Feni"]
+
+
+def print_len(cities):
+    print(len(cities))
+
+
+print_len(cities)
---
+++ Chapter-06 (Functions & Recursion)/ListInSingleLine.py
@@ -0,0 +1,11 @@
+cities = ["Dhaka", "Tangail", "Mirzapur", "Chittagoan", "Comilla", "Feni"]
+
+print(cities[0], end=" ")
+print(cities[1], end=" ")
+
+
+def print_len(cities):
+    print(cities)
+
+
+# print_len(cities)

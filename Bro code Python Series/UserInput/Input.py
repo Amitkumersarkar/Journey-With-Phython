ed708db25555c1b8cfb4b2c54680913ed7ff55ad
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-10-17 17:41:07 +0600

    that's all about user input

---
+++ Bro code Python Series/UserInput/Input.py
@@ -0,0 +1,7 @@
+name = input("What is your name ? : ")
+age = input("How old are you ? :")
+age = int(age)
+age = age+1
+print(f"Hello , i am {name}")
+print(f"Happy birthday !")
+print(f"Your {age} years old")

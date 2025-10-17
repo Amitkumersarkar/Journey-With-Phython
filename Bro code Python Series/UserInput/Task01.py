f64e32a44d2df89b596818857f91f62e8859ec79
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-10-17 17:59:06 +0600

    added new practice task

---
+++ Bro code Python Series/UserInput/Task01.py
@@ -0,0 +1,8 @@
+# calculate the area of the rectangle
+
+length = input("Enter the length : ")
+width = input("Enter the width : ")
+length = float(length)
+width = float(width)
+area = length * width
+print(f"thea area of the rectangle is : {area} cm")

a2134feeaf2b94198e2d775a58da0e01486540bf
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-10-17 17:15:43 +0600

    thats all about variable in py

--- Bro code Python Series/variable.py
+++ Bro code Python Series/variable.py
@@ -1,2 +1,24 @@
+# string
+
 first_name = "Amit"
+food = "Rice and Curry"
+email = "amitsarkar.dev.bd@gmail.com"
 print(f"Hello {first_name}")
+print(f"Do You Like {food}")
+print(f"Is it your mail : {email}")
+
+# integer
+
+age = 23
+print(f"i am {age} old")
+
+# float
+
+gpa = 4.67
+print(f"my hsc gpa is {gpa} out of 5.0")
+
+# boolean
+
+is_student = True
+
+print(f"are you student {is_student}")
